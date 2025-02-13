cc5da0a27e0cee739b372393d690cd487ae81e63
willll <464311+willll@users.noreply.github.com>
2025-02-13 16:23:15 -0500

    disable c++ std lib

--- build-gcc-final.sh
+++ build-gcc-final.sh
@@ -26,7 +26,7 @@ export CDIR=$PWD
 	--with-gnu-as --with-gnu-ld --with-gcc  \
     --with-stabs --disable-shared --disable-threads \
 	--disable-multilib --disable-tls --disable-libssp --disable-lto \
-	--disable-newlib --disable-multilib --disable-libstdcxx-v3 \
+	--disable-newlib --disable-multilib --disable-libstdcxx \
 	--with-cpu=m68000 --disable-werror \
 	--program-prefix=${PROGRAM_PREFIX} ${GCC_FINAL_FLAGS}
 
